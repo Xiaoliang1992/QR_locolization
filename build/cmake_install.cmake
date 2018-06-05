9f2b75e3ef7b73dd536c32c2b360b322ae35c067
Xiaoliang Wang <wangxiaoliang1992@gmail.com>
2018-06-05 16:33:44 +0800

    first

---
+++ build/cmake_install.cmake
@@ -0,0 +1,87 @@
+# Install script for directory: /home/xiaoliang/APP/apriltags
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/xiaoliang/APP/apriltags/build/lib/libapriltags.a")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/AprilTags" TYPE FILE FILES
+    "/home/xiaoliang/APP/apriltags/AprilTags/TagDetection.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/XYWeight.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Quad.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Edge.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/TagDetector.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Tag36h11_other.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Tag36h9.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Segment.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Gaussian.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/UnionFindSimple.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Tag36h11.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/TagFamily.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/GLineSegment2D.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Tag16h5_other.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Tag16h5.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/GrayModel.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/GLine2D.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/FloatImage.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/MathUtil.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Gridder.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Homography33.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Tag25h7.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/Tag25h9.h"
+    "/home/xiaoliang/APP/apriltags/AprilTags/pch.h"
+    )
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/xiaoliang/APP/apriltags/build/lib/pkgconfig/apriltags.pc")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/home/xiaoliang/APP/apriltags/build/example/cmake_install.cmake")
+
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/xiaoliang/APP/apriltags/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ pod-build/CMakeFiles/apriltags.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xiaoliang/APP/apriltags/src/Edge.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Edge.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/FloatImage.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/FloatImage.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/GLine2D.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLine2D.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/GLineSegment2D.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GLineSegment2D.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/Gaussian.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Gaussian.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/GrayModel.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/GrayModel.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/Homography33.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Homography33.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/MathUtil.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/MathUtil.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/Quad.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Quad.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/Segment.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/Segment.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/TagDetection.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetection.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/TagDetector.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagDetector.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/TagFamily.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/TagFamily.cc.o"
+  "/home/xiaoliang/APP/apriltags/src/UnionFindSimple.cc" "/home/xiaoliang/APP/apriltags/pod-build/CMakeFiles/apriltags.dir/src/UnionFindSimple.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../build/include"
+  "../AprilTags"
+  "../."
+  "/opt/local/include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
